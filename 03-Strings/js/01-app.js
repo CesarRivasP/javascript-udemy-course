aedb84280f513de46f3e1d2beaeafda500c4d103
cesarrivas10 <cesarjose58@gmail.com>
2020-08-26 16:19:24 -0400

    Add string lesson

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,13 @@
+const product1 = String('Monitor Asus ROG');
+
+console.log(product1);
+
+// Crear un nuevo objeto de tipo string
+const product = new String('Monitor AliemWare');
+
+console.log(product);
+
+// indicar pulgadas sin que marque error
+const testProduct = "Monitor de 28\"";
+
+console.log(testProduct);
