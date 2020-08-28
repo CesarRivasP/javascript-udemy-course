7d8c0e763e7249c53dfa8eb1a51a99c11bf658cb
cesarrivas10 <cesarjose58@gmail.com>
2020-08-28 18:03:48 -0400

    Create numbers lesson 5

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,14 @@
+let score = 5;
+
+ // Incremento de 1 en 1
+score++;
+
+// Para que incremente de una vez sin tener que volver a llamar
+++score;
+
+console.log(score);
+
+score--;
+--score;
+
+console.log(score);
