90660306d370c1083e176cc28e214198f2f3a46c
cesarrivas10 <cesarjose58@gmail.com>
2020-09-01 18:10:27 -0400

    Add comparing operators lesson

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,12 @@
+const numberOne = 20;
+const numberTwo = "20";
+const numberFour = 30;
+
+
+// Comparador No estricto
+console.log(numberOne == numberTwo);  //true
+// solo se fija en el valor
+
+// Comparador Estricto
+console.log(numberOne === numberTwo);  //false
+// se fija en el valor y en el tipo de dato
