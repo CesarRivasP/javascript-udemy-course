1d8d5875dd8a6a31f5cf04727d6f58a6d77a0dcc
cesarrivas10 <cesarjose58@gmail.com>
2020-09-02 21:25:11 -0400

    Add booleans lesson

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,5 @@
+// Otra forma de crear booleanos
+// Este tipo de constructores por medio de new crean objetos, no otros valores primitivos
+const boolean = new Boolean(true);
+
+console.log(boolean);
